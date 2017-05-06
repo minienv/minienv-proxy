00386dc5062b2b419a6914c8ad8c59b7ef523666
Mark Watson <mark.watson.x@gmail.com>
2017-05-06 02:10:40 -0500

    Handle error on starup

--- main.go
+++ main.go
@@ -9,10 +9,13 @@ import (
 
 func main() {
 	if len(os.Args) != 2 {
-		log.Fatalf("Usage: %s <port> <targetHost>", os.Args[0])
+		log.Fatalf("Usage: %s <port>", os.Args[0])
 	}
 	if _, err := strconv.Atoi(os.Args[1]); err != nil {
 		log.Fatalf("Invalid port: %s (%s)\n", os.Args[1], err)
 	}
-	http.ListenAndServe(":"+os.Args[1], NewReverseProxy(os.Getenv("DC_TARGET_HOST")))
+	err := http.ListenAndServe(":"+os.Args[1], NewReverseProxy(os.Getenv("DC_TARGET_HOST")))
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
